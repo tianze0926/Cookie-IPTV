4784a1009cd405eaa8d2198971b43aca157a83a6
zhoutz19 <zhoutz19@mails.tsinghua.edu.cn>
2022-01-24 21:08:11 +0800

    add capacitor

---
+++ android/app/src/main/java/com/tz039e/iptv/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tz039e.iptv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tz039e.iptv',
+  appName: 'CookieIPTV',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
